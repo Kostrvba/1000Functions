c8961d06ee371479b3a19b74c0a95cec4f17c89b
Kostrvba <kostrubiec50@gmail.com>
2023-03-16 21:38:40 +0100

    delete not working function

--- 1000Functions.py
+++ 1000Functions.py
@@ -774,13 +774,13 @@ def inherited(message):
 
 print(inherited("od taty"))
 
+
 def repeat_str(repeat: int, string: str) -> str:
     return string * repeat
 
+
 print(repeat_str(3, "HI"))
 
-def get_status(is_busy):
-    status = "busy" if is_busy else "available"
-    return {"status": status}
 
-#90!!!!!!!!!
+
+# 89!!!!!!!!!
